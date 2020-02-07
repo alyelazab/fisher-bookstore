739dd609270a005f19cc5a4d00e1b2ff0e8d3783
Aly ElAzab <elazab.1@osu.edu>
2020-02-07 12:25:07 -0500

    authors feature init

---
+++ src/authors/Authors.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Authors.css";
+
+export default function Authors() {
+    return (
+    <div className = "Authors">
+        <div className="lander">
+            <h1>The Authors</h1>
+            <p>The Authors part of the code.</p>
+        </div>
+    </div>
+    );
+}

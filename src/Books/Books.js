4feea3bf69680e649767a6e5822eac1491d44e01
Aly ElAzab <elazab.1@osu.edu>
2020-02-06 21:47:03 -0500

    init books button

---
+++ src/Books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Books() {
+    return (
+    <div className = "Books">
+        <div className="lander">
+            <h1>The Books</h1>
+            <p>The Books part of the code.</p>
+        </div>
+    </div>
+    );
+}
--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
+import Books from "./Books/Books";
 
 export default function Routes() {
     return(
         <Switch>
             <Route path="/" exact component = {Home} />
+            <Route path="/" exact component = {Books} />
+        
         </Switch>
+        
+        
     );
 }
